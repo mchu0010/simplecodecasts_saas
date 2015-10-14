30caa9bdbbd24a34f851d5814dd600fa99fae16d
Mike Chu <m.chu90@gmail.com>
2015-10-14 17:01:01 -0500

    comments for default email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'm.chu90@gmail.com'
+  default to: 'm.chu90@gmail.com' #default to this email for now
 
   def contact_email(name, email, body)
     @name = name
