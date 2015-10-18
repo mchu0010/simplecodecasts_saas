35511bf3f6a7c6204a3341ed96d4f7c4f9a210f6
Mike Chu <m.chu90@gmail.com>
2015-10-18 17:22:35 -0500

    update devise route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registration: 'users/registrations'}
   resources :contacts
   get '/about' => 'pages#about'
   root 'pages#home'
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #   get 'roducts/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
