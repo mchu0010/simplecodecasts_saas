a4f8e3292ca3bcca8ba66995214c5068959afc05
Mike Chu <m.chu90@gmail.com>
2015-10-19 15:36:08 -0500

    added profile new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,9 @@
 class ProfilesController < ApplicationController
+  def new
+    # form where a user can fill out their own profile
+    @user = User.find(params[:user_id])
+    @variables = params[:hello]
+    @profile = @user.build_profile
+  end
   
 end
